f0b725fdd53f1475eda6b826760fdee88169b50e
dealbisac <chand.dipendra19@gmail.com>
2024-01-01 14:46:14 +0100

    feature: setup all the user action route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -66,9 +66,41 @@ export async function POST(req: Request) {
             photo: image_url,
         }
 
-        // const newUser = await createUser(user);
+        const newUser = await createUser(user);
+
+        if (newUser) {
+            await clerkClient.users.updateUserMetadata(id, {
+                publicMetadata: {
+                    userId: newUser._id
+                }
+            })
+        }
+
+        return NextResponse.json({ message: 'OK', user: newUser })
     }
 
+    if (eventType === 'user.updated') {
+        const { id, image_url, first_name, last_name, username } = evt.data
+
+        const user = {
+            firstName: first_name,
+            lastName: last_name,
+            username: username!,
+            photo: image_url,
+        }
+
+        const updatedUser = await updateUser(id, user)
+
+        return NextResponse.json({ message: 'OK', user: updatedUser })
+    }
+
+    if (eventType === 'user.deleted') {
+        const { id } = evt.data
+
+        const deletedUser = await deleteUser(id!)
+
+        return NextResponse.json({ message: 'OK', user: deletedUser })
+    }
 
     return new Response('', { status: 200 })
 }
