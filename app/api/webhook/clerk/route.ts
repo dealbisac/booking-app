ae6e67c5ef31a1c9a13829e8d1587afddf9b0a1d
dealbisac <chand.dipendra19@gmail.com>
2024-01-01 14:19:01 +0100

    feature: add route action of new user created

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
 
@@ -51,8 +54,21 @@ export async function POST(req: Request) {
     const { id } = evt.data;
     const eventType = evt.type;
 
-    console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-    console.log('Webhook body:', body)
+    if (eventType === 'user.created') {
+        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+        const user = {
+            clerkId: id,
+            email: email_addresses[0].email_address,
+            username: username!,
+            firstName: first_name,
+            lastName: last_name,
+            photo: image_url,
+        }
+
+        // const newUser = await createUser(user);
+    }
+
 
     return new Response('', { status: 200 })
 }
