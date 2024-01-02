27f50887b4fd0dd0b6b427ea95e2c0c4e749938b
dealbisac <chand.dipendra19@gmail.com>
2024-01-02 22:36:54 +0100

    feature: start create event page

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,11 @@
+const CreateEvent = () => {
+    return (
+        <section>
+            <h3 className="wrapper h3-bold text-center sm:text-left">
+                Create Event
+            </h3>
+        </section>
+    )
+}
+
+export default CreateEvent
