622d087fc6a53628bfaec21066bab3aeb3dd8cdc
dealbisac <chand.dipendra19@gmail.com>
2024-01-03 16:06:26 +0100

    refactor: add extra font weight

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-poppins',
 })
 
