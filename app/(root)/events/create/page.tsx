2d653b0a60a1cbea1923224b8d610795b132f858
dealbisac <chand.dipendra19@gmail.com>
2024-01-04 11:47:27 +0100

    start setup event form

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,10 +1,20 @@
+import { auth } from "@clerk/nextjs";
+
 const CreateEvent = () => {
+    const { sessionClaims } = auth();
+
+    const userId = sessionClaims?.userId as string;
+
     return (
-        <section>
-            <h3 className="wrapper h3-bold text-center sm:text-left">
-                Create Event
-            </h3>
-        </section>
+        <>
+            <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+                <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+            </section>
+
+            <div className="wrapper my-8">
+                EventForm
+            </div>
+        </>
     )
 }
 
