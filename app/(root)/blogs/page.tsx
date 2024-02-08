3381a367a3d482ac325baa83b0f42abff57f936c
dealbisac <chand.dipendra19@gmail.com>
2024-02-08 23:08:08 +0100

    add blogs rough data

--- app/(root)/blogs/page.tsx
+++ app/(root)/blogs/page.tsx
@@ -4,7 +4,11 @@ const Blogs = () => {
     return (
         <section>
             <h1>Blogs</h1>
-            TODO: Add blogs
+            {/* List the list of the blogs */}
+            <div className="section">
+                <h2>Blog 1</h2>
+                <p>Blog 1 content</p>
+            </div>
         </section>
     )
 }
