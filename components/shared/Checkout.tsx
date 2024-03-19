1459594d26b6a9b2cf0b0125c583c59890011872
dealbisac <chand.dipendra19@gmail.com>
2024-03-19 17:16:40 +0100

    update stripe publishable key

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -5,7 +5,7 @@ import { IEvent } from '@/lib/database/models/event.model';
 import { Button } from '../ui/button';
 import { checkoutOrder } from '@/lib/actions/order.actions';
 
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
     useEffect(() => {
