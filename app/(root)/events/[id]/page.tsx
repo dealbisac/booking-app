c24597f8c1f64f5c3dce8ae534d5df7caee1910a
dealbisac <chand.dipendra19@gmail.com>
2024-03-19 11:22:23 +0100

    update link and help text of event description

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -71,9 +71,13 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
                         </div>
 
                         <div className="flex flex-col gap-2">
-                            <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+                            <p className="p-bold-20 text-grey-600">Event Description:</p>
                             <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
-                            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{event.url}</p>
+                            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+                                <a href={event.url}>
+                                {event.url}
+                                </a> 
+                            </p>
                         </div>
                     </div>
                 </div>
